68ef3aac1fddef03a4d2f6337d2a46f9b2b4b3cc
Jugal Kishor Sahu <jugalkishorsahu5@gmail.com>
2024-07-20 14:02:56 +0530

    init commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <img src={logoImg} alt="Quiz logo" />
-      <h1>ReactQuiz</h1>
+      <h1>Celebrity Quiz</h1>
     </header>
   );
 }
