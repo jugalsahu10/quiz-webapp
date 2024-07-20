6af08523c65316e626654dead1d70a06bfd7e465
Jugal Kishor Sahu <jugalkishorsahu5@gmail.com>
2024-07-20 13:59:48 +0530

    init commit

---
+++ src/questions.js
@@ -0,0 +1,102 @@
+export default [
+  {
+    id: 'q1',
+    text: 'Who is known as the "King of Bollywood"?',
+    answers: [
+      'Shah Rukh Khan.',
+      'Salman Khan.',
+      'Aamir Khan.',
+      'Hrithik Roshan.',
+    ],
+  },
+  {
+    id: 'q2',
+    text: 'Which actress is referred to as the "Queen of Bollywood"?',
+    answers: [
+      'Deepika Padukone.',
+      'Kareena Kapoor.',
+      'Aishwarya Rai Bachchan.',
+      'Priyanka Chopra.',
+    ],
+  },
+  {
+    id: 'q3',
+    text: 'Which Indian cricketer is married to Bollywood actress Anushka Sharma?',
+    answers: [
+      'Virat Kohli.',
+      'MS Dhoni.',
+      'Rohit Sharma.',
+      'Sachin Tendulkar.',
+    ],
+  },
+  {
+    id: 'q4',
+    text: 'Which Bollywood actor is known for his role in the movie "3 Idiots"?',
+    answers: [
+      'Aamir Khan.',
+      'Salman Khan.',
+      'Shah Rukh Khan.',
+      'Akshay Kumar.',
+    ],
+  },
+  {
+    id: 'q5',
+    text: 'Who is the daughter of Bollywood actor Anil Kapoor?',
+    answers: [
+      'Sonam Kapoor.',
+      'Alia Bhatt.',
+      'Katrina Kaif.',
+      'Kareena Kapoor.',
+    ],
+  },
+  {
+    id: 'q6',
+    text: 'Which actor is known for the portrayal of "Kabir Singh"?',
+    answers: [
+      'Shahid Kapoor.',
+      'Ranveer Singh.',
+      'Sushant Singh Rajput.',
+      'Varun Dhawan.',
+    ],
+  },
+  {
+    id: 'q7',
+    text: 'Which Bollywood actress made her Hollywood debut with the movie "Baywatch"?',
+    answers: [
+      'Priyanka Chopra.',
+      'Deepika Padukone.',
+      'Aishwarya Rai Bachchan.',
+      'Kangana Ranaut.',
+    ],
+  },
+  {
+    id: 'q8',
+    text: 'Which legendary actor is known as "Big B" in Bollywood?',
+    answers: [
+      'Amitabh Bachchan.',
+      'Rajesh Khanna.',
+      'Dharmendra.',
+      'Shatrughan Sinha.',
+    ],
+  },
+  {
+    id: 'q9',
+    text: 'Who played the lead role in the movie "Dangal"?',
+    answers: [
+      'Aamir Khan.',
+      'Salman Khan.',
+      'Akshay Kumar.',
+      'Shah Rukh Khan.',
+    ],
+  },
+  {
+    id: 'q10',
+    text: 'Which Bollywood actress is known for her role in the movie "Queen"?',
+    answers: [
+      'Kangana Ranaut.',
+      'Alia Bhatt.',
+      'Anushka Sharma.',
+      'Sonakshi Sinha.',
+    ],
+  },
+];
